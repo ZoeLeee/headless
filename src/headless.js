2430983e82c4b05c2260400e5293e6f2b32a0640
Zoe <lizhou1021@qq.com>
2018-03-09 23:40:01 +0800

    爬截图

---
+++ src/headless.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer');
+const config=require('./config/config');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.baidu.com');
+  await page.screenshot({path: `${config.path}/${Date.now()}.png`});
+
+  await browser.close();
+})();
